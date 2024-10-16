1d0fd9944f0dd226c80d145db03c77c29144a43c
zaiphirion <zaiphirion@zaiphirion.zaiphirion>
2024-10-16 15:01:34 -0700

    ...

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -6,6 +6,7 @@ class t_theme {
 
     this.textColor1 = "enigma_text_color_1";
     this.borderColor1 = "enigma_border_color_1";
+    this.borderColor2 = "enigma_border_color_2";
     this.bgColor1 = "enigma_bg_color_1";
     this.bgColor2 = "enigma_bg_color_2";
     this.bgColor3 = "enigma_bg_color_3";
@@ -19,10 +20,11 @@ const Header = ({ children = '' }) => {
   return <div className={`
     ${ theme.bgColor2 }
     ${ theme.textColor1 }
+    m-1
     px-2 py-1
-    rounded-t-lg
     font-bold
     text-sm
+    tracking-wider
   `}>
     { children }
   </div>;
@@ -33,11 +35,12 @@ const Footer = ({ children = '' }) => {
   return <div className={`
     ${ theme.bgColor2 }
     ${ theme.textColor1 }
+    m-1
     px-2 py-1
-    rounded-b-lg
     font-bold
     text-sm
     text-right
+    tracking-wider
   `}>
     { children }
   </div>;
@@ -53,10 +56,26 @@ const Com = ({
     ${ theme.borderColor1 }
     ${ theme.bgColor3 } 
     p-px
-    rounded-lg
   `}>
     { header ? <Header>{ title }</Header> : '' }
     <TextBody>Test</TextBody>
+    { footer ? <Footer>{ footer }</Footer> : '' }
+  </div>;
+} 
+
+const Com2 = ({
+  header = true, 
+  footer = '',
+  title = '',
+  children = '' } ) => {
+
+  return <div className={`
+    ${ theme.borderColor1 }
+    ${ theme.bgColor3 } 
+    p-px
+  `}>
+    { header ? <Header>{ title }</Header> : '' }
+    <TextBody2>&gt; _</TextBody2>
     { footer ? <Footer>{ footer }</Footer> : '' }
   </div>;
 } 
@@ -82,17 +101,25 @@ const TextBody = ({ children }) => {
   </div>
 }
 
+const TextBody2 = ({ children }) => {
+
+  return <div className="flex items-end mx-1 my-1 px-2 py-2 h-[100px] text-sm font-bold bg-black">
+    { children }
+  </div>
+}
+
 const Panel = () => {
 
   return <div className={`
     ${ theme.borderColor1 }
-    ${ theme.bgColor1 }
-    my-8 mx-auto
-    p-2
+    ${ theme.bgColor2 }
+    mx-auto
     w-[800px]
   `}>
+    <Header>Profile - Johnny L. de Alba</Header>
+    <div className="p-2">
       <Col>
-        <Com title="Console" footer="Expand" />
+        <Com2 title="Console" footer="Expand" />
       </Col>
       <Col>
         <Com title="Bulletin" footer="Previous Entries" />
@@ -105,12 +132,15 @@ const Panel = () => {
           <Com title="About Me" />
         </Col2>
       </div>
+    </div>
   </div>;
 }
 
 export const Page = () => {
 
-  return <Panel />;
+  return <div className="h-screen bg-[#12152c] p-8">
+    <Panel />
+  </div>;
 }
 
 export default Page;
